783a327edce41514e9556b4404c9c2229c9d85bf
mvpvo-iscteiulpt <mvpvo@iscte-iul.pt>
2017-05-17 14:28:19 +0100

    sells cap remade

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -245,25 +245,15 @@ public class MicroServer implements MicroTraderServer {
 		
 
 		Order o = msg.getOrder();
+		Set<Order> clientOrders = orderMap.get(o.getNickname());
 		
 		if(o.getNumberOfUnits() < MAX_UNITS_NUMBER){
 			JOptionPane.showMessageDialog(null, "Insufficient number of units. Order rejected.", "Warning", 2);
 		}
+		else if( o.isSellOrder() && manySells( clientOrders ) ){
+			JOptionPane.showMessageDialog(null, "Too many sell orders from same client. Order rejected.", "Warning", 2);
+		}
 		else{
-			
-			Set<Order> clientOrders = orderMap.get(o.getNickname());
-			
-			if(o.isSellOrder()){
-				int sellsNumber = 0;
-				for(Order i : clientOrders){
-					if(i.isSellOrder()){
-						sellsNumber++;
-					}
-					if(sellsNumber >= MAX_SELLS_NUMBER){
-						throw new ServerException("Too many sell orders from same client. Maximum is " + MAX_SELLS_NUMBER + ". Order rejected.");
-					}
-				}
-			}
 			Set<Order> temp = new HashSet<Order>();
 			for(Order a : clientOrders){
 				if(o.getStock().equals(a.getStock())){
@@ -445,6 +435,21 @@ public class MicroServer implements MicroTraderServer {
 									";" + o.getPricePerUnit()+
 									"]");
 			}
+		}
+	}
+	
+	private boolean manySells(Set<Order> clientOrders){
+		int sells = 0;
+		for(Order o : clientOrders){
+			if(o.isSellOrder()){
+				sells++;
+			}
+		}
+		if(sells >= MAX_SELLS_NUMBER){
+			return true;
+		}
+		else{
+			return false;
 		}
 	}
 	
