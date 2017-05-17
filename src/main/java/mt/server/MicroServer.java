3a6f504bc198e27d967fee1c968ce9365e2cbd2f
mvpvo-iscteiulpt <mvpvo@iscte-iul.pt>
2017-05-17 17:35:04 +0100

    server running pop up

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -104,6 +104,8 @@ public class MicroServer implements MicroTraderServer {
 		this.serverComm = serverComm;
 
 		ServerSideMessage msg = null;
+		
+		JOptionPane.showMessageDialog(null, "Server running.", "U.S. Version", 1);
 		while ((msg = serverComm.getNextMessage()) != null) {
 			ServerSideMessage.Type type = msg.getType();
 			
